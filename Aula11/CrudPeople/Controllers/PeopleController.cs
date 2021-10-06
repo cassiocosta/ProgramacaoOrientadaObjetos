ab55b17ce5b7b35141a1d36267dd8f786153d995
Cassio Costa <cassio.hc@gmail.com>
2021-10-05 21:30:59 -0300

    iterando aula 11

--- Aula11/CrudPeople/Controllers/PeopleController.cs
+++ Aula11/CrudPeople/Controllers/PeopleController.cs
@@ -27,6 +27,40 @@ namespace CrudPeople.Controllers
             }
             return _repository.GetAll();
         }
+
+        [HttpGet("{Id}")] // localhost:5000/people/10
+        public Person Get(int id)
+        {
+            return _repository.GetById(id);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody]Person person)
+        {
+            _repository.Create(person);
+            return Ok(person);
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody]Person person)
+        {
+            _repository.Update(person);
+            return Ok( new {
+                message = "Atualizado com sucesso.",
+                codeHttp=201,
+                objetoCreated = person
+            });
+        }
+
+        [HttpDelete("{Id}")]
+        public IActionResult Delete(int id)
+        {
+            _repository.Delete(id);
+            return Ok(new{
+                message="Person foi deletado com sucesso.",
+                id = id
+            });
+        }
          
     }
 }
